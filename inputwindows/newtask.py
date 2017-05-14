ad90d87582f97569577f1c27a60d079833d93ce3
cayc3 <cayce@tutanota.com>
2017-05-14 08:44:30 +0700

    Update newtask.py

--- inputwindows/newtask.py
+++ inputwindows/newtask.py
@@ -1,5 +1,5 @@
-from PyQt4.QtCore import pyqtSignature
-from PyQt4.QtGui import QDialog, QMessageBox
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QDialog, QMessageBox
 
 from .Ui_newtask import Ui_Dialog
 
@@ -7,9 +7,9 @@ class NewTask(QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         QDialog.__init__(self, parent)
         self.setupUi(self)
-    
+
     db = None
-    
+
     def setup(self):
         cursor = self.db.cursor()
         cursor.execute("select category from task_categories")
@@ -18,22 +18,22 @@ class NewTask(QDialog, Ui_Dialog):
             items.append(row[0])
         self.cmbCategory.addItems(items)
         cursor.close()
-        
+
     def validate(self):
         result = False
 
         if self.cmbCategory.currentText() != "" and self.txtName.text() != "" and self.txtDescription.toPlainText() != "" and self.txtFileName.text() != "":
             result = True
-        
+
         return result
-        
-    @pyqtSignature("")
+
+    @pyqtSlot()
     def on_btnSave_clicked(self):
         if self.validate():
             self.accept()
         else:
             QMessageBox.information(self, "Information", "You need to fill in all the fields")
-    
-    @pyqtSignature("")
+
+    @pyqtSlot()
     def on_btnCancel_clicked(self):
         self.reject()
