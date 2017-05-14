11eab888cbb725579efd9cc24cd990648fd83597
cayc3 <cayce@tutanota.com>
2017-05-14 08:43:26 +0700

    Update confirmation.py

--- inputwindows/confirmation.py
+++ inputwindows/confirmation.py
@@ -1,7 +1,6 @@
-from PyQt4.QtCore import pyqtSignature
-from PyQt4.QtGui import QDialog
-from PyQt4 import QtGui,  QtCore
-from PyQt4.QtCore import QString
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QDialog
+from PyQt5 import QtGui,  QtCore
 
 from .Ui_confirmation import Ui_Dialog
 
@@ -10,15 +9,15 @@ class wndConfirmation(QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         QDialog.__init__(self, parent)
         self.setupUi(self)
-        
+
         logoPixmap = QtGui.QPixmap(QString.fromUtf8('images/confirm.png'))
         logoScaledPixmap = logoPixmap.scaled(self.lblImage.size(),  QtCore.Qt.KeepAspectRatio)
         self.lblImage.setPixmap(logoScaledPixmap)
-    
-    @pyqtSignature("")
+
+    @pyqtSlot()
     def on_btnYes_clicked(self):
         self.accept()
-    
-    @pyqtSignature("")
+
+    @pyqtSlot()
     def on_btnNo_clicked(self):
         self.reject()
