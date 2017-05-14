c6017174a7ac9e873a7f10e7ac2741e136387c78
cayc3 <cayce@tutanota.com>
2017-05-14 08:46:47 +0700

    Update textinput.py

--- inputwindows/textinput.py
+++ inputwindows/textinput.py
@@ -1,5 +1,5 @@
-from PyQt4.QtCore import pyqtSignature
-from PyQt4.QtGui import QDialog, QMessageBox
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QDialog, QMessageBox
 
 from .Ui_textinput import Ui_Dialog
 
@@ -8,14 +8,14 @@ class wndTextInput(QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         QDialog.__init__(self, parent)
         self.setupUi(self)
-    
-    @pyqtSignature("")
+
+    @pyqtSlot()
     def on_btnOk_clicked(self):
         if self.txtDomain.text() == "" or self.txtLootFileName.text() == "":
             QMessageBox.information(self, "Information", "You need to fill in both fields")
         else:
             self.accept()
-    
-    @pyqtSignature("")
+
+    @pyqtSlot()
     def on_btnCancel_clicked(self):
         self.reject()
