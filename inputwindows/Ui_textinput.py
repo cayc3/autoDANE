319f3a87d9e084481b2a115d19dba10539a8f3bd
cayc3 <cayce@tutanota.com>
2017-05-14 09:42:49 +0700

    Update Ui_textinput.py

--- inputwindows/Ui_textinput.py
+++ inputwindows/Ui_textinput.py
@@ -7,7 +7,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -16,52 +16,52 @@ except AttributeError:
         return s
 
 try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
+    _encoding = QtWidgets.QApplication.UnicodeUTF8
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+        return QtWidgets.QApplication.translate(context, text, disambig)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(423, 172)
         Dialog.setSizeGripEnabled(True)
-        self.gridLayout = QtGui.QGridLayout(Dialog)
+        self.gridLayout = QtWidgets.QGridLayout(Dialog)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
         self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
-        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
-        self.label = QtGui.QLabel(Dialog)
+        self.label = QtWidgets.QLabel(Dialog)
         self.label.setMinimumSize(QtCore.QSize(75, 0))
         self.label.setObjectName(_fromUtf8("label"))
         self.horizontalLayout_2.addWidget(self.label)
-        self.txtDomain = QtGui.QLineEdit(Dialog)
+        self.txtDomain = QtWidgets.QLineEdit(Dialog)
         self.txtDomain.setObjectName(_fromUtf8("txtDomain"))
         self.horizontalLayout_2.addWidget(self.txtDomain)
         self.verticalLayout_2.addLayout(self.horizontalLayout_2)
-        self.horizontalLayout_3 = QtGui.QHBoxLayout()
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
-        self.label_2 = QtGui.QLabel(Dialog)
+        self.label_2 = QtWidgets.QLabel(Dialog)
         self.label_2.setMinimumSize(QtCore.QSize(75, 0))
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.horizontalLayout_3.addWidget(self.label_2)
-        self.txtLootFileName = QtGui.QLineEdit(Dialog)
+        self.txtLootFileName = QtWidgets.QLineEdit(Dialog)
         self.txtLootFileName.setObjectName(_fromUtf8("txtLootFileName"))
         self.horizontalLayout_3.addWidget(self.txtLootFileName)
         self.verticalLayout_2.addLayout(self.horizontalLayout_3)
-        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout_2.addItem(spacerItem)
-        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem1)
-        self.btnOk = QtGui.QPushButton(Dialog)
+        self.btnOk = QtWidgets.QPushButton(Dialog)
         self.btnOk.setObjectName(_fromUtf8("btnOk"))
         self.horizontalLayout.addWidget(self.btnOk)
-        self.btnCancel = QtGui.QPushButton(Dialog)
+        self.btnCancel = QtWidgets.QPushButton(Dialog)
         self.btnCancel.setObjectName(_fromUtf8("btnCancel"))
         self.horizontalLayout.addWidget(self.btnCancel)
         self.verticalLayout_2.addLayout(self.horizontalLayout)
@@ -80,10 +80,9 @@ class Ui_Dialog(object):
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     Dialog = QtGui.QDialog()
     ui = Ui_Dialog()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
-
