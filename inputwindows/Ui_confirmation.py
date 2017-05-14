3d4c8a978102ae77e71eea954a639bfee16f2c48
cayc3 <cayce@tutanota.com>
2017-05-14 08:35:24 +0700

    Update Ui_confirmation.py

--- inputwindows/Ui_confirmation.py
+++ inputwindows/Ui_confirmation.py
@@ -6,7 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -15,12 +15,12 @@ except AttributeError:
         return s
 
 try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
+    _encoding = QtWidgets.QApplication.UnicodeUTF8
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+        return QtWidgets.QApplication.translate(context, text, disambig)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -28,23 +28,23 @@ class Ui_Dialog(object):
         Dialog.resize(485, 275)
         Dialog.setSizeGripEnabled(True)
         Dialog.setModal(True)
-        self.gridLayout = QtGui.QGridLayout(Dialog)
+        self.gridLayout = QtWidgets.QGridLayout(Dialog)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.lblImage = QtGui.QLabel(Dialog)
+        self.lblImage = QtWidgets.QLabel(Dialog)
         self.lblImage.setMinimumSize(QtCore.QSize(221, 221))
         self.lblImage.setMaximumSize(QtCore.QSize(221, 221))
         self.lblImage.setObjectName(_fromUtf8("lblImage"))
         self.horizontalLayout.addWidget(self.lblImage)
-        self.label_2 = QtGui.QLabel(Dialog)
+        self.label_2 = QtWidgets.QLabel(Dialog)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.horizontalLayout.addWidget(self.label_2)
         self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 2)
-        self.btnYes = QtGui.QPushButton(Dialog)
+        self.btnYes = QtWidgets.QPushButton(Dialog)
         self.btnYes.setObjectName(_fromUtf8("btnYes"))
         self.gridLayout.addWidget(self.btnYes, 1, 0, 1, 1)
-        self.btnNo = QtGui.QPushButton(Dialog)
+        self.btnNo = QtWidgets.QPushButton(Dialog)
         self.btnNo.setObjectName(_fromUtf8("btnNo"))
         self.gridLayout.addWidget(self.btnNo, 1, 1, 1, 1)
 
@@ -61,10 +61,9 @@ class Ui_Dialog(object):
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     Dialog = QtGui.QDialog()
     ui = Ui_Dialog()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
-
